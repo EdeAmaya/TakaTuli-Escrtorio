54052d106aa463916f7685dde8c9ee0c595b9a86
EdeAmaya <edenilsonamaya10@gmail.com>
2024-08-22 11:35:26 -0600

    Inicio del Fin

---
+++ src/Modelo/Hospedaje.java
@@ -0,0 +1,46 @@
+package Modelo;
+
+
+public class Hospedaje {
+    
+    String UUID_Hospedaje;
+    String Nombre_Hospedaje;
+    double Precio_Hospedaje;
+    String Detalles_Hospedaje;
+    
+    
+    public String getUUID_Hospedaje() {
+        return UUID_Hospedaje;
+    }
+
+    public void setUUID_Hospedaje(String UUID_Hospedaje) {
+        this.UUID_Hospedaje = UUID_Hospedaje;
+    }
+
+    public String getNombre_Hospedaje() {
+        return Nombre_Hospedaje;
+    }
+
+    public void setNombre_Hospedaje(String Nombre_Hospedaje) {
+        this.Nombre_Hospedaje = Nombre_Hospedaje;
+    }
+
+    public double getPrecio_Hospedaje() {
+        return Precio_Hospedaje;
+    }
+
+    public void setPrecio_Hospedaje(double Precio_Hospedaje) {
+        this.Precio_Hospedaje = Precio_Hospedaje;
+    }
+
+    public String getDetalles_Hospedaje() {
+        return Detalles_Hospedaje;
+    }
+
+    public void setDetalles_Hospedaje(String Detalles_Hospedaje) {
+        this.Detalles_Hospedaje = Detalles_Hospedaje;
+    }
+    
+  
+    
+}
